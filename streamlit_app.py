24b0ea218711ed6c5122e7ab341d4d54b6aaf4bc
AhmadA1i <mianahmadali301@gmail.com>
2024-08-11 20:26:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,36 @@
 import streamlit as st
-st.title("Machine learning App")
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("🤖Machine learning App")
 st.info("This app builds a machine learning model!")
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
+st.write('**X**')
+X_raw = df.drop('species',axis=1)
+X_raw
+
+st.write('**y**')
+y_raw = df['species']
+y_raw
+
+with st.expander('Data Visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+with st.sidebar:
+  st.header("Input Features")
+  island = st.selectbox('Island',('Biscoe', 'Dream', Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length-mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 27000.0, 6300,0, 4207.0)
+  gender = st.selectbox('Gender, ('male','female'))
+
+  # Create a DataFrame for the input features
+  
+  
